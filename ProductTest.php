6d3f265dd82668da4da51b73266abdfdd7708f9c
dev <y2ksoft@gmail.com>
2021-02-27 19:12:54 +1300

    Updated Test cases

--- ProductTest.php
+++ ProductTest.php
@@ -2,13 +2,33 @@
 
 
 use Shopping\Product;
-use PHPUnit\Framework\TestCase;
+require './vendor/autoload.php';
 
-class ProductTest extends TestCase
+/**
+ * Note : This test cases have not been tested due to php 8 version phpunit/pear error
+ *
+ * Class ProductTest
+ */
+class ProductTest extends PHPUnit_Framework_TestCase
 {
-
-    public function testSetQuantity()
+    /**
+     * Test both products vales are same
+     */
+    public function testFindBothProductSame()
     {
+        // get the first Product from product List
+        $shoppingCart = new ShoppingCartController();
+        $cart = new Cart();
+        $productList = $shoppingCart->viewProductList();
+        $cart->addProduct($productList[0]);
+        $checkProduct = $cart->getProductById($productList[0]['uniqueId']);
+        \PHPUnit\Framework\assertSame($productList[0], $checkProduct);
+    }
 
+    public function testProductQuantityIncrement()
+    {
+        $product = new \Shopping\Product('Sl', 'Sledgehammer',125.75);
+        $product->setQuantity(1);
+        \PHPUnit\Framework\assertEquals(2, $product->getQuantity());
     }
 }
